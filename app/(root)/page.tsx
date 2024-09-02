71ee486ee87fc9ca1c1436ac28c022e8981cc337
leejeff0319 <leejeff0319@gmail.com>
2024-09-02 03:29:07 -0400

    Implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import HeaderBox from '@/components/ui/HeaderBox'
-import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 
 const Home = () => {
-    const loggedIn = { firstName: 'Jeff' };
+  const loggedIn = { firstName: "Jeff", lastName:"Me", email:"user@email.com"};
 
-    return (
-        <section className="home">
-            <div className="home-content">
-                <header className="home-header">
-                    <HeaderBox
-                        type="greeting"
-                        title="Welcome"
-                        user={loggedIn?.firstName || 'Guest'}
-                        subtext="Access and manage your account and transactions efficiently."
-                    />
-                </header>
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-                <TotalBalanceBox
-                    accounts={{}}
-                    totalBanks={1}
-                    totalCurrentBalance={1250.35}
-                />
-            </div>
-        </section>
-    )
-}
+          <TotalBalanceBox
+            accounts={{}}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
