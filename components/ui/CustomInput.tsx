1c271818bcd8443ce1fad10dc3db64038c5fc805
leejeff0319 <leejeff0319@gmail.com>
2024-09-16 20:29:18 -0400

    finish authorization UI

--- components/ui/CustomInput.tsx
+++ components/ui/CustomInput.tsx
@@ -15,9 +15,11 @@ import {
 import { Input } from "@/components/ui/input";
 import { authFormSchema } from "@/lib/utils";
 
+const formSchema = authFormSchema("sign-up")
+
 interface CustomInput{
-    control: Control<z.infer<typeof authFormSchema>>,
-    name: FieldPath<z.infer<typeof authFormSchema>>,
+    control: Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
     label: string,
     placeholder: string,
 }
